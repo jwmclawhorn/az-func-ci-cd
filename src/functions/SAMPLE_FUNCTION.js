d7ea6f536f8550bc5853e51f547a154a65cc5152
Justin McLawhorn <jwmclawhorn@pm.me>
2026-06-10 20:30:45 -0400

    initial

---
+++ src/functions/SAMPLE_FUNCTION.js
@@ -0,0 +1,10 @@
+import { app } from '@azure/functions';
+
+app.http('SAMPLE_FUNCTION', {
+    methods: ['GET', 'POST'],
+    authLevel: 'anonymous',
+    handler: async (request, context) => {
+        context.log('HTTP trigger function processed a request.');
+        return { body: 'Hello, world!' };
+    }
+});
