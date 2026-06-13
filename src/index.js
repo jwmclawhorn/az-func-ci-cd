aa901cf16c5dfd6352ec40ce0f77041fcc55acd9
Justin McLawhorn <jwmclawhorn@pm.me>
2026-06-13 16:59:47 -0400

    feat: add 2nd sample function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './functions/SAMPLE_FUNCTION.js';
 import './functions/SAMPLE_AXIOS_FUNCTION.js';
 import './functions/SAMPLE_IDENTITY_FUNCTION.js';
+import './functions/SAMPLE_FUNCTION_2.js';
